d35846817ca75a972b65ea991704ced4a17b2ae3
Knut Andreas Meyer <knut.andreas.meyer@gmail.com>
2019-06-05 17:58:06 +0200

    Added output from external script to a specific log file

--- src/sim/simulation_mod.f90
+++ src/sim/simulation_mod.f90
@@ -259,7 +259,7 @@ implicit none
     ! 4) efile (eext_simnr.txt) gives the file to which the scalar error should be written
     ! 5) evecfile (evec_simnr.txt) gives the file to which the error vector should be written (if requested)
     command = trim(f_data%sim(simnr)%ext_cmd%script) // ' ' // eout_str // ' ' // int2str(f_data%glob%resnr) // ' ' &
-                                             // trim(mpar_file) // ' ' // trim(efile) // ' ' // trim(evecfile)
+                                             // trim(mpar_file) // ' ' // trim(efile) // ' ' // trim(evecfile) // ' ' // '&>> extsim.log'
     
     ! Write material parameters to default file 'mpar.txt'
     open(newunit=mpar_fid, file=trim(mpar_file), status='replace', action='write', IOSTAT=status)
