a8bcc6416056ae76d3e1568f52765ae1178f93be
Knut Andreas Meyer <knut.andreas.meyer@gmail.com>
2019-09-18 21:24:23 +0200

    Added specific installation file for cluster

---
+++ scripts/install_cluster.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+cd ../..    # Go outside matmodfit folder
+
+# Load modules
+module load intel CMake Python
+
+# Install nlopt
+echo "Installing nlopt"
+git clone git://github.com/stevengj/nlopt
+cd nlopt
+mkdir build
+cd build
+CC=icc CXX=icpc FC=ifort cmake -DCMAKE_INSTALL_PREFIX="$(pwd)/../install" ..
+make
+make install
+cd ../..
+export nlopt_path="$(pwd)/nlopt/install/lib64/libnlopt.so"
+
+# Install matmodfit
+echo "Installing matmodfit"
+# git clone https://github.com/KnutAM/matmodfit.git (assume already downloaded)
+cd matmodfit/scripts
+chmod 744 install_matmodfit.sh
+
+# Set the replies to avoid user interaction
+echo "1" > replies.txt      # compiler=ifort (Replace for first to ensure empty file)
+echo "yes" >> replies.txt   # default directory (Append for the remaining answers)
+echo "yes" >> replies.txt   # add paths to .bash_profile/.bashrc/.profile files
+
+# Run the installation script
+./install_matmodfit.sh < replies.txt    # Use the lines in replies.txt as answers. 
+
+rm replies.txt # Remove the replies.txt file to avoid clutter
+
+echo "Running test cases"
+cd ../testing
+python run_test_cases.py
+python clean_test_cases.py
+
